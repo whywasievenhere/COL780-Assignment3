95d26e371d32da2dab556a012c31be6cd6341204
As4s1n <behera.sarthak3@gmail.com>
2021-11-10 21:24:10 +0530

    Added plotter.py

--- utils.py
+++ utils.py
@@ -14,9 +14,5 @@ def plot_PR_across_ious(hog_cv2_pr, hog_svm_pr, frcnn_pr, hog_cv2_re, hog_svm_re
     plt.plot(hog_cv2_re, hog_cv2_pr, 'o-', label='hog_cv2')
     plt.plot(hog_svm_re, hog_svm_pr, 'o-', label='hog_svm')
     plt.plot(frcnn_re, frcnn_pr, 'o-', label='frcnn')
-
-    for x, y in zip(hog_cv2_re, hog_cv2_pr):
-        
-
     plt.legend()
     plt.savefig('PrRe_across_ious.png', bbox_inches='tight')
